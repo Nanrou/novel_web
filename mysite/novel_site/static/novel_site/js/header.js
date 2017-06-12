7d9bc040a1e62b2e4bbafa0db36eedf0d5fe75b8
Nanrou <kkkcomkkk@qq.com>
2017-06-12 16:34:54 +0800

    add fav.ico, sethome func and addfavorite func

--- mysite/novel_site/static/novel_site/js/header.js
+++ mysite/novel_site/static/novel_site/js/header.js
@@ -7,6 +7,52 @@ function setCookie(c_name,value,expiredays)//设置cookie
     //escape是编码字符串（编码URL有专门的encodeURL），toGMTString已经弃用，改成UTC，paths的默认位置就是/
 }
 
+function SetHome(obj,vrl)
+    {
+        try
+        {
+                obj.style.behavior='url(#default#homepage)';obj.setHomePage(vrl);
+        }
+        catch(e){
+                if(window.netscape) {
+                        try {
+                                netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect"); 
+                        } 
+                        catch (e) { 
+                                alert("此操作被浏览器拒绝！\n请在浏览器地址栏输入“about:config”并回车\n然后将[signed.applets.codebase_principal_support]设置为'true'"); 
+                        }
+                        var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch);
+                        prefs.setCharPref('browser.startup.homepage',vrl);
+                 }
+        }
+    }
+
+function addFavorite() {
+    var url = window.location;
+    var title = document.title;
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.indexOf("360se") > -1) {
+        alert("由于360浏览器功能限制，请按 Ctrl+D 手动收藏！");
+    }
+    else if (ua.indexOf("msie 8") > -1) {
+        window.external.AddToFavoritesBar(url, title); //IE8
+    }
+    else if (document.all) {
+  try{
+   window.external.addFavorite(url, title);
+  }catch(e){
+   alert('您的浏览器不支持,请按 Ctrl+D 手动收藏!');
+  }
+    }
+    else if (window.sidebar) {
+        window.sidebar.addPanel(title, url, "");
+    }
+    else {
+  alert('您的浏览器不支持,请按 Ctrl+D 手动收藏!');
+    }
+}
+
+
 function getCookie(c_name)//拿到某个name的值
 {
     if (document.cookie.length>0){//若没有cookie则直接返回空
