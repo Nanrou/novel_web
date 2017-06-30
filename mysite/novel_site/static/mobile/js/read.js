443de14065e391c058cd7af53b4c285e8775680c
Nanrou <kkkcomkkk@qq.com>
2017-06-30 17:13:59 +0800

    update mobile js part

--- mysite/novel_site/static/mobile/js/read.js
+++ mysite/novel_site/static/mobile/js/read.js
@@ -5,112 +5,81 @@ $(window).load(function(){  // 页面加载完后就设置cookie
 		setCookie('lastread_url', window.location.pathname, 365);
 });
 
+function setSize(size) {  // 设置字体大小 
+	$('#fontbig').css("background-color", '#ECF0F0');
+	$('#fontmiddle').css("background-color", '#ECF0F0');
+	$('#fontsmall').css("background-color", '#ECF0F0');
+	if (size == 'big'){
+		$("#nr").css("fontSize", '26px');
+		$('#fontbig').css("background-color", '#A7A7A7');
+	}else if (size == 'middle'){
+		$("#nr").css("fontSize", '22px');
+		$('#fontmiddle').css("background-color", '#A7A7A7');
+	}else {
+		$("#nr").css("fontSize", '16px');
+		$('#fontsmall').css("background-color", '#A7A7A7');
+	}
+	setCookie("m_fontsize", size, 365);
+}
 
-function LastRead(){this.bookList="bookList"}
-LastRead.prototype={
-	set:function(bid,tid,title,texttitle,author,sortname){
-		if(!(bid&&tid&&title&&texttitle&&author&&sortname))return;
-		var v=bid+'#'+tid+'#'+title+'#'+texttitle+'#'+author+'#'+sortname;
-		this.setItem(bid,v);
-		this.setBook(bid)
-	},
-	get:function(k){
-		return this.getItem(k)?this.getItem(k).split("#"):"";
-	},
-	remove:function(k){
-		this.removeItem(k);
-		this.removeBook(k)
-	},
-	setBook:function(v){
-		var reg=new RegExp("(^|#)"+v);
-		var books =	this.getItem(this.bookList);
-		if(books==""){
-			books=v
-			}
-		 else{
-			 if(books.search(reg)==-1){
-				 books+="#"+v
-				 }
-			 else{
-				  books.replace(reg,"#"+v)
-				 }
-			 }
-		this.setItem(this.bookList,books)
-	},
-	getBook:function(){
-		var v=this.getItem(this.bookList)?this.getItem(this.bookList).split("#"):Array();
-		var books=Array();
-		if(v.length){
-			for(var i=0;i<v.length;i++){
-				var tem=this.getItem(v[i]).split('#');
-				if (tem.length>3)books.push(tem);
-				}
-			}
-		return books
-	},
-	removeBook:function(v){
-	    var reg=new RegExp("(^|#)"+v);
-		var books=this.getItem(this.bookList);
-		if(!books){
-			books=""
-			}
-		 else{
-			 if(books.search(reg)!=-1){
-			      books=books.replace(reg,"")
-				 }
-			 }
-		this.setItem(this.bookList,books)
-	},
-	setItem:function(k,v){
-		if(!!window.localStorage){
-			localStorage.setItem(k,v);
-		}
-		else{
-			var expireDate=new Date();
-			  var EXPIR_MONTH=30*24*3600*1000;
-			  expireDate.setTime(expireDate.getTime()+12*EXPIR_MONTH)
-			  document.cookie=k+"="+encodeURIComponent(v)+";expires="+expireDate.toGMTString()+"; path=/";
-		}
-	},
-	getItem:function(k){
-		var value=""
-		var result=""
-		if(!!window.localStorage){
-			result=window.localStorage.getItem(k);
-			 value=result||"";
-		}
-		else{
-			var reg=new RegExp("(^| )"+k+"=([^;]*)(;|\x24)");
-			var result=reg.exec(document.cookie);
-			if(result){
-				value=decodeURIComponent(result[2])||""}
-		}
-		return value
-	},
-	removeItem:function(k){
-		if(!!window.localStorage){
-		 window.localStorage.removeItem(k);
-		}
-		else{
-			var expireDate=new Date();
-			expireDate.setTime(expireDate.getTime()-1000)
-			document.cookie=k+"= "+";expires="+expireDate.toGMTString()
-		}
-	},
-	removeAll:function(){
-		if(!!window.localStorage){
-		 window.localStorage.clear();
-		}
-		else{
-		var v=this.getItem(this.bookList)?this.getItem(this.bookList).split("#"):Array();
-		var books=Array();
-		if(v.length){
-			for( i in v ){
-				var tem=this.removeItem(v[k])
-				}
-			}
-			this.removeItem(this.bookList)
-		}
+function setHuyan() {  // 护眼模式
+	$("#nr").css("color", '#000');
+	$("#nr_title").css("color", '#000');
+	$("#lightdiv").css("background-color", '#ECF0F0');
+	$("#lightdiv").text('关灯');
+	setCookie("m_yejian", 0, 365);
+
+	var switch_ = getCookie('m_huyan')
+	if (switch_ == 1){
+		$("#nr_body").css("background-color", '#FBF6EC');
+		$("#huyandiv").css("background-color", '#ECF0F0');
+		setCookie("m_huyan", 0, 365);
+	}
+	else {
+		$("#nr_body").css("background-color", '#DCECD2');
+		$("#huyandiv").css("background-color", '#A7A7A7');
+		setCookie("m_huyan", 1, 365);
 	}
 }
-window.lastread = new LastRead();
+
+function setYejian() {  // 夜间模式
+	$("#huyandiv").css("background-color", '#ECF0F0');
+	setCookie("m_huyan", 0, 365);
+
+	var switch_ = getCookie('m_yejian')
+	if (switch_ == 1){
+		$("#nr_body").css("background-color", '#FBF6EC');
+		$("#nr").css("color", '#000');
+		$("#nr_title").css("color", '#000');
+		$("#lightdiv").css("background-color", '#ECF0F0');
+		$("#lightdiv").text('关灯');
+		setCookie("m_yejian", 0, 365);
+	}
+	else {
+		$("#nr_body").css("background-color", '#32373B');
+		$("#nr").css("color", '#CCCCCC');
+		$("#nr_title").css("color", '#CCCCCC');
+		$("#lightdiv").css("background-color", '#A7A7A7');
+		$("#lightdiv").text('开灯');
+		setCookie("m_yejian", 1, 365);
+	}
+}
+
+$(document).ready(function(){  // 页面初始化
+	var size = getCookie("m_fontsize");
+	if (size){
+		setSize(size);
+	}else {
+		setSize('middle');
+	};
+	var huyan = getCookie("m_huyan");
+	if (huyan==1){
+		setCookie("m_huyan", 0, 365);
+		setHuyan();
+	};
+	var yejian = getCookie("m_yejian");
+	if (yejian==1){
+		setCookie("m_yejian", 0, 365);
+		setYejian();
+	};
+})
