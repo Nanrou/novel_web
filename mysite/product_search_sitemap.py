1a3dcddc0eeeb85ab1a8d993047eb70e81b85bf4
Nanrou <kkkcomkkk@qq.com>
2017-07-31 15:16:24 +0800

    update sitemap

--- mysite/product_search_sitemap.py
+++ mysite/product_search_sitemap.py
@@ -25,6 +25,7 @@ xml_txt = '''
                 <articleSection>{book_name}</articleSection>
                 <headline>{chapter_name}</headline>
                 <dateModified>{update_datetime}</dateModified>
+                <url><![CDATA[http://superxiaoshuo.com{chapter_url}]]></url>
             </newestChapter>
             <chapter><headline>{chapter_name}</headline></chapter>
             <endingType>{category}</endingType>
@@ -36,12 +37,13 @@ xml_txt = '''
 with open('search_sitemap.xml', 'w', encoding='utf-8') as wf:
     wf.write('<?xml version="1.0" encoding="UTF-8"?><urlset>')
 
-for i in range(1, 20):
+for i in range(1, 21):
     book = InfoTable.objects.get(id=i)
     with open('search_sitemap.xml', 'a', encoding='utf-8') as af:
         af.write(xml_txt.format(id=i, update_time=str(book.update_time).replace(' ', 'T'), book_name=book.title,
                                 author=book.author, category=book.category, status=book.status,
-                                chapter_name=book.latest_chapter, update_datetime=str(book.update_time).split(' ')[0]))
+                                chapter_name=book.latest_chapter, update_datetime=str(book.update_time).split(' ')[0],
+                                chapter_url=book.latest_chapter.get_absolute_url(),))
 else:
     with open('search_sitemap.xml', 'a', encoding='utf-8') as f:
         f.write('</urlset>')
